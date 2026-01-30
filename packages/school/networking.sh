5696b17483c9b0a80314baf44b9c7cadf662b308
Pierre Olivier Martel <pomartel@coderubik.com>
2026-01-30 16:14:31 -0500

    networking

--- packages/school/networking.sh
+++ packages/school/networking.sh
@@ -4,8 +4,21 @@ omarchy-pkg-add networkmanager network-manager-applet wpa_supplicant
 wifi_backend_conf="/etc/NetworkManager/conf.d/wifi_backend.conf"
 if [ ! -f "$wifi_backend_conf" ]; then
     echo "Configuring NetworkManager to use wpa_supplicant as the Wi-Fi backend..."
-    printf "[device]\nwifi.backend=wpa_supplicant\n" | sudo tee "$wifi_backend_conf" >/dev/null
+    printf "[device]\nwifi.backend=wpa_supplicant\n" | sudo tee "$wifi_backend_conf"
 fi
+
+# Only add the conf if the file doesn't already exist
+mac_address_conf="/etc/NetworkManager/conf.d/00-mac-address.conf"
+if [ ! -f "$mac_address_conf" ]; then
+    echo "Configuring NetworkManager MAC address settings..."
+    printf "[device]\n" \
+           "wifi.scan-rand-mac-address=no\n" \
+           "\n" \
+           "[connection]\n" \
+           "wifi.cloned-mac-address=preserve\n" \
+           "ethernet.cloned-mac-address=preserve\n" | sudo tee "$mac_address_conf"
+fi
+
 
 if systemctl is-active --quiet iwd; then
     echo "Disabling iwd to avoid conflicts with NetworkManager..."
