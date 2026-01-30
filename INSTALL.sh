ccf46de6897f2d3931f5899d0495f40b47aefd06
Pierre Olivier Martel <pomartel@coderubik.com>
2026-01-30 15:36:27 -0500

    fix

--- INSTALL.sh
+++ INSTALL.sh
@@ -26,7 +26,6 @@ fi
 ./remove-default-apps.sh
 ./set-default-font.sh
 ./add-suspend-to-system-menu.sh
-./set-power-profile-rule.sh
 ./configure-hibernation.sh
 
 echo "All install scripts completed."
