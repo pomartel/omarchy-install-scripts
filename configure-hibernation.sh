134db029bcfe02cc7230fed2707b838a7bba2b1b
Pierre Olivier Martel <pomartel@coderubik.com>
2026-02-19 15:58:25 -0500

    Simplify

--- configure-hibernation.sh
+++ configure-hibernation.sh
@@ -1,25 +1,14 @@
 #!/bin/bash
 
-# Check if hibernation is already configured
-MKINITCPIO_CONF="/etc/mkinitcpio.conf.d/omarchy_resume.conf"
-if [ -f "$MKINITCPIO_CONF" ] && grep -q "^HOOKS+=(resume)$" "$MKINITCPIO_CONF"; then
-  exit 0
-else
-  omarchy-hibernation-setup
-
-  sudo mkdir -p /etc/systemd/sleep.conf.d /etc/systemd/logind.conf.d
-
-  read -r -d '' SLEEP_CONTENT <<'EOF'
+read -r -d '' SLEEP_CONTENT <<'EOF'
 [Sleep]
 HibernateDelaySec=24h
 HibernateOnACPower=no
 EOF
-  printf '%s\n' "$SLEEP_CONTENT" | sudo tee /etc/systemd/sleep.conf.d/hibernate.conf >/dev/null
+printf '%s\n' "$SLEEP_CONTENT" | sudo tee /etc/systemd/sleep.conf.d/hibernate.conf >/dev/null
 
-  read -r -d '' LID_CONTENT <<'EOF'
+read -r -d '' LID_CONTENT <<'EOF'
 [Login]
 HandleLidSwitch=suspend-then-hibernate
 EOF
-  printf '%s\n' "$LID_CONTENT" | sudo tee /etc/systemd/logind.conf.d/lid.conf >/dev/null
-
-fi
+printf '%s\n' "$LID_CONTENT" | sudo tee /etc/systemd/logind.conf.d/lid.conf >/dev/null
--- copy-sudoers-rules.sh
+++ copy-sudoers-rules.sh
@@ -2,13 +2,9 @@
 
 SUDOERS_FILE="/etc/sudoers.d/custom-sudoers-rules"
 
-if ! sudo test -f $SUDOERS_FILE; then
-  read -r -d '' SUDOERS_CONTENT <<'EOF'
+read -r -d '' SUDOERS_CONTENT <<'EOF'
 Defaults timestamp_timeout=60
 Defaults !tty_tickets
 EOF
-
-  printf '%s\n' "$SUDOERS_CONTENT" | sudo tee "$SUDOERS_FILE" >/dev/null
-  sudo chmod 600 "$SUDOERS_FILE"
-  echo "sudoers rules updated: $SUDOERS_FILE"
-fi
+printf '%s\n' "$SUDOERS_CONTENT" | sudo tee "$SUDOERS_FILE" >/dev/null
+sudo chmod 600 "$SUDOERS_FILE"
