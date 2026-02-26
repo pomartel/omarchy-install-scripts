b7807d3ac57381c7c64c7022b694413e8cb1f9be
Pierre Olivier Martel <pomartel@coderubik.com>
2026-02-26 10:05:25 -0500

    Update create-dropbox-symlinks.sh

--- configs/create-dropbox-symlinks.sh
+++ configs/create-dropbox-symlinks.sh
@@ -6,6 +6,11 @@ fi
 
 ln -sfn "${HOME}/Dropbox/Pictures" "${HOME}/Pictures"
 ln -sfn "${HOME}/Dropbox/Cours" "${HOME}/Cours"
-
 ln -sfn "${HOME}/Dropbox/CV" "${HOME}/Documents/CV"
-ln -sfn "${HOME}/Dropbox/Omarchy" "${HOME}/Documents/Omarchy"
+
+if [[ "${INSTALL_TARGET}" == "home" ]]; then
+  ln -sfn "${HOME}/Dropbox/Code-Rubik" "${HOME}/Documents/Code-Rubik"
+  ln -sfn "${HOME}/Dropbox/Finances" "${HOME}/Documents/Finances"
+  ln -sfn "${HOME}/Dropbox/Maison" "${HOME}/Documents/Maison"
+  ln -sfn "${HOME}/Dropbox/Omarchy" "${HOME}/Documents/Omarchy"
+fi
