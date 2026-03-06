22d10c7863c39a35a1130ea1fac3ba82d2657350
Pierre Olivier Martel <pomartel@coderubik.com>
2026-03-06 17:51:43 -0500

    Update fix-plocate.sh

--- configs/fix-plocate.sh
+++ configs/fix-plocate.sh
@@ -10,5 +10,13 @@ AccuracySec=6h
 Persistent=false
 EOF
 
-# sudo systemctl daemon-reload
-# sudo systemctl restart plocate-updatedb.timer
+sudo install -d /etc/systemd/system/plocate-updatedb.service.d/
+
+sudo tee /etc/systemd/system/plocate-updatedb.service.d/config-file.conf >/dev/null <<'EOF'
+[Service]
+ExecStart=
+ExecStart=/usr/bin/updatedb --config-file=/home/po/.config/plocate/updatedb.conf
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl restart plocate-updatedb.timer
