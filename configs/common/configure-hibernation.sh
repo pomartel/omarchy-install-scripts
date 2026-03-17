5068b739b8a0d007a1389b1a51946a809b3962db
Pierre Olivier Martel <pomartel@coderubik.com>
2026-03-17 16:44:21 -0400

    hibernation

--- configs/common/configure-hibernation.sh
+++ configs/common/configure-hibernation.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-sudo tee /etc/systemd/sleep.conf.d/hibernate.conf >/dev/null <<EOF
-[Sleep]
-HibernateDelaySec=24h
-HibernateOnACPower=no
-EOF
+# sudo tee /etc/systemd/sleep.conf.d/hibernate.conf >/dev/null <<EOF
+# [Sleep]
+# HibernateDelaySec=24h
+# HibernateOnACPower=no
+# EOF
 
-sudo tee /etc/systemd/logind.conf.d/lid.conf >/dev/null <<EOF
-[Login]
-HandleLidSwitch=suspend-then-hibernate
-EOF
+# sudo tee /etc/systemd/logind.conf.d/lid.conf >/dev/null <<EOF
+# [Login]
+# HandleLidSwitch=suspend-then-hibernate
+# EOF
+
+sudo rm -f /etc/systemd/sleep.conf.d/hibernate.conf
+sudo rm -f /etc/systemd/logind.conf.d/lid.conf
