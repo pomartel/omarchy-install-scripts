a5a75220d1003eef2855af6ff1aa9f52683cd9e1
Pierre Olivier Martel <pomartel@coderubik.com>
2026-04-07 21:44:48 -0400

    Update enable-fingerprint-reader.sh

--- configs/home/enable-fingerprint-reader.sh
+++ configs/home/enable-fingerprint-reader.sh
@@ -3,7 +3,7 @@
 if ! grep -Rqs 'pam_fprintd\.so' /etc/pam.d; then
   omarchy-setup-fingerprint
   fprintd-enroll -f left-index-finger "$USER"
-fi
 
-# Disable fingerprint reader in sudo
-sudo sed -i 's/^auth[[:space:]]\+sufficient[[:space:]]\+pam_fprintd\.so$/# &/' /etc/pam.d/sudo
+  # Disable fingerprint reader in sudo
+  sudo sed -i 's/^auth[[:space:]]\+sufficient[[:space:]]\+pam_fprintd\.so$/# &/' /etc/pam.d/sudo
+fi
