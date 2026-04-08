394f19f37180ccaa9ae94b8e3f2ad6259d6e76e7
Pierre Olivier Martel <pomartel@coderubik.com>
2026-04-08 09:22:31 -0400

    Use target-specific brightness in udev rules
    - Introduce target-specific brightness values for battery and AC power
    - Use INSTALL_TARGET to set 50%/100% for school builds, else 25%/80%
    - Update udev rule to apply the computed brightness via brightnessctl

--- configs/common/set-power-rules.sh
+++ configs/common/set-power-rules.sh
@@ -5,10 +5,18 @@ SUBSYSTEM=="power_supply", ATTR{type}=="Mains", ATTR{online}=="0", RUN+="/usr/bi
 SUBSYSTEM=="power_supply", ATTR{type}=="Mains", ATTR{online}=="1", RUN+="/usr/bin/powerprofilesctl set balanced"
 EOF
 
+if [ "$INSTALL_TARGET" = "school" ]; then
+  bat_brightness="50%"
+  ac_brightness="100%"
+else
+  bat_brightness="25%"
+  ac_brightness="80%"
+fi
+
 rule_file="/etc/udev/rules.d/99-display-brightness.rules"
 cat <<EOF | sudo tee "$rule_file" >/dev/null
-ACTION=="change", SUBSYSTEM=="power_supply", ATTR{type}=="Mains", ATTR{online}=="0", RUN+="/usr/bin/brightnessctl -c backlight set 25%"
-ACTION=="change", SUBSYSTEM=="power_supply", ATTR{type}=="Mains", ATTR{online}=="1", RUN+="/usr/bin/brightnessctl -c backlight set 80%"
+ACTION=="change", SUBSYSTEM=="power_supply", ATTR{type}=="Mains", ATTR{online}=="0", RUN+="/usr/bin/brightnessctl -c backlight set $bat_brightness"
+ACTION=="change", SUBSYSTEM=="power_supply", ATTR{type}=="Mains", ATTR{online}=="1", RUN+="/usr/bin/brightnessctl -c backlight set $ac_brightness"
 EOF
 
 # Switch power profile to power-saver on low battery
