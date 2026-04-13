85a6e77aafdd8a0feac2c522a78621126507a884
Pierre Olivier Martel <pomartel@coderubik.com>
2026-04-13 10:33:52 -0400

    Delete set-locale.sh

--- configs/home/set-locale.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-current_locale=$(locale | grep LANG=)
-target_locale="LANG=en_CA.UTF-8"
-
-if [[ "$current_locale" != "$target_locale" ]]; then
-  echo "Setting system locale to $target_locale..."
-  sudo localectl set-locale "$target_locale"
-fi
