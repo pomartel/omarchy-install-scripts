7d3d59bd389b7d01d777fa45bf3562315c5892d5
Pierre Olivier Martel <pomartel@coderubik.com>
2026-04-15 16:06:26 -0400

    Update set-power-rules.sh

--- configs/common/set-power-rules.sh
+++ configs/common/set-power-rules.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo tee "/etc/udev/rules.d/99-power-profile.rules" >/dev/null <<EOF
-SUBSYSTEM=="power_supply", ATTR{type}=="Mains", ATTR{online}=="0", RUN+="/usr/bin/powerprofilesctl set balanced"
-SUBSYSTEM=="power_supply", ATTR{type}=="Mains", ATTR{online}=="1", RUN+="/usr/bin/powerprofilesctl set balanced"
+cat <<EOF | sudo tee "/etc/udev/rules.d/99-power-profile.rules" >/dev/null
+SUBSYSTEM=="power_supply", ATTR{type}=="Mains", ATTR{online}=="0", RUN+="/usr/bin/systemd-run --no-block --collect --unit=omarchy-power-profile-battery --property=After=power-profiles-daemon.service $HOME/bin/omarchy-powerprofiles-set battery"
+SUBSYSTEM=="power_supply", ATTR{type}=="Mains", ATTR{online}=="1", RUN+="/usr/bin/systemd-run --no-block --collect --unit=omarchy-power-profile-ac --property=After=power-profiles-daemon.service $HOME/bin/omarchy-powerprofiles-set ac"
 EOF
 
 if [ "$INSTALL_TARGET" = "school" ]; then
@@ -15,9 +15,12 @@ fi
 
 rule_file="/etc/udev/rules.d/99-display-brightness.rules"
 cat <<EOF | sudo tee "$rule_file" >/dev/null
-ACTION=="change", SUBSYSTEM=="power_supply", ATTR{type}=="Mains", ATTR{online}=="0", RUN+="/usr/bin/brightnessctl -c backlight set $bat_brightness"
-ACTION=="change", SUBSYSTEM=="power_supply", ATTR{type}=="Mains", ATTR{online}=="1", RUN+="/usr/bin/brightnessctl -c backlight set $ac_brightness"
+ACTION=="change", SUBSYSTEM=="power_supply", ATTR{type}=="Mains", ATTR{online}=="0", RUN+="/usr/bin/brightnessctl -c backlight set $bat_brightness%"
+ACTION=="change", SUBSYSTEM=="power_supply", ATTR{type}=="Mains", ATTR{online}=="1", RUN+="/usr/bin/brightnessctl -c backlight set $ac_brightness%"
 EOF
+
+# Reload udev rules
+sudo udevadm control --reload-rules
 
 # Switch power profile to power-saver on low battery
 service=powerprofile-low-battery.timer
