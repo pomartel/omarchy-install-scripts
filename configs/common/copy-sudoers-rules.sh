e52afd017af5a97dc7a0d222e31c499673865202
Pierre Olivier Martel <pomartel@coderubik.com>
2026-04-22 12:42:58 -0400

    sudoers rule

--- configs/common/copy-sudoers-rules.sh
+++ configs/common/copy-sudoers-rules.sh
@@ -5,6 +5,8 @@ sudoers_file="/etc/sudoers.d/custom-sudoers-rules"
 sudo tee "$sudoers_file" >/dev/null <<EOF
 Defaults timestamp_timeout=60
 Defaults !tty_tickets
+
+po ALL=(root) NOPASSWD: /usr/bin/tee /sys/devices/system/cpu/cpu*/cpufreq/energy_performance_preference
 EOF
 
 sudo chmod 600 "$sudoers_file"
