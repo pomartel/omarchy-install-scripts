1d9dc28287859c9fa4788db8dbde048f55c68563
Pierre Olivier Martel <pomartel@coderubik.com>
2026-05-01 13:19:11 -0400

    Update configure-bluetooth-wake.sh

--- configs/common/configure-bluetooth-wake.sh
+++ configs/common/configure-bluetooth-wake.sh
@@ -2,14 +2,11 @@
 
 printf '%s\n' 'ACTION=="add|change", SUBSYSTEM=="usb", ATTR{bDeviceClass}=="e0", ATTR{bDeviceSubClass}=="01", ATTR{bDeviceProtocol}=="01", ATTR{power/wakeup}="enabled"' | sudo tee /etc/udev/rules.d/70-bluetooth-keyboard-wake.rules >/dev/null
 
-if awk '$1 == "CNVW" && $3 ~ /\*disabled/' /proc/acpi/wakeup >/dev/null; then
-  printf '%s\n' CNVW | sudo tee /proc/acpi/wakeup >/dev/null
+if grep -qE '^CNVW[[:space:]].*\*disabled' /proc/acpi/wakeup; then
+  printf '%s\n' CNVW | sudo tee /proc/acpi/wakeup >/dev/null 2>&1
 fi
 
-for wake in \
-  "$(readlink -f /sys/class/bluetooth/hci0/device/../power/wakeup 2>/dev/null)" \
-  "$(readlink -f /sys/class/bluetooth/hci0/device/../../power/wakeup 2>/dev/null)"; do
-  if [[ -n "$wake" && -e "$wake" ]]; then
-    printf '%s\n' enabled | sudo tee "$wake" >/dev/null
-  fi
+for wake in /sys/class/bluetooth/hci0/device/{../,../../}power/wakeup; do
+  [[ -e "$wake" ]] || continue
+  printf '%s\n' enabled | sudo tee "$wake" >/dev/null
 done
