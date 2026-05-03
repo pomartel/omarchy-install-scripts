747bf0c61726f5d446cf4c7fb1da6309ae3079f4
Pierre Olivier Martel <pomartel@coderubik.com>
2026-05-03 15:37:56 -0400

    powerprofile

--- configs/common/configure-auto-powerprofile.sh
+++ configs/common/configure-auto-powerprofile.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cat <<EOF | sudo tee "/etc/udev/rules.d/99-power-profile.rules" >/dev/null
-SUBSYSTEM=="power_supply", ATTR{type}=="Mains", ATTR{online}=="0", RUN+="/usr/bin/systemd-run --no-block --collect --unit=omarchy-power-profile-battery --property=After=power-profiles-daemon.service $HOME/bin/omarchy-powerprofiles-set battery"
-SUBSYSTEM=="power_supply", ATTR{type}=="Mains", ATTR{online}=="1", RUN+="/usr/bin/systemd-run --no-block --collect --unit=omarchy-power-profile-ac --property=After=power-profiles-daemon.service $HOME/bin/omarchy-powerprofiles-set ac"
+SUBSYSTEM=="power_supply", ATTR{type}=="Mains", RUN+="/usr/bin/systemd-run --no-block --collect --unit=omarchy-power-profile --property=After=power-profiles-daemon.service $HOME/bin/omarchy-powerprofiles-set"
+SUBSYSTEM=="power_supply", ATTR{type}=="USB", RUN+="/usr/bin/systemd-run --no-block --collect --unit=omarchy-power-profile --property=After=power-profiles-daemon.service $HOME/bin/omarchy-powerprofiles-set"
 EOF
 
 sudo udevadm control --reload-rules
