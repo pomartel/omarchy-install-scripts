35092e08c74b2ca78fef54dd2a6c976cec80f0c2
Pierre Olivier Martel <pomartel@coderubik.com>
2026-09-07 07:55:42 -0400

    Update clone-git-projects.sh

--- configs/common/clone-git-projects.sh
+++ configs/common/clone-git-projects.sh
@@ -21,10 +21,8 @@ if [ "$INSTALL_TARGET" = "lenovo" ]; then
   clone_if_missing "git@github.com:pomartel/poll-app.git"
   clone_if_missing "git@github.com:pomartel/fbpoll.co.git" "poll-app.com"
   clone_if_missing "git@github.com:pomartel/coderubik.com.git"
+  clone_if_missing "git@github.com:pomartel/alexa-anylist-sync.git"
 fi
 
 clone_if_missing "git@github.com:pomartel/sudomarchy"
-clone_if_missing "git@github.com:pomartel/intemporel"
-clone_if_missing "git@github.com:pomartel/idle-power"
-clone_if_missing "git@github.com:pomartel/qs-yadm"
 clone_if_missing "git@git.dti.crosemont.quebec:pmartel/markdown-export.git" "markdown"
