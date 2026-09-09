14ef9359828f56198b7dc676167ffaf82700fca5
Pierre Olivier Martel <pomartel@coderubik.com>
2026-09-09 15:54:28 -0400

    Install Time Machine only on Lenovo

--- configs/common/install-plugins.sh
+++ configs/common/install-plugins.sh
@@ -22,4 +22,8 @@ ensure_omarchy_plugin "omarchy-todoist" "pomartel/omarchy-todoist"
 ensure_omarchy_plugin "qs-yadm" "pomartel/qs-yadm"
 ensure_omarchy_plugin "crmne.hyprmoncfg" "crmne/omarchy-hyprmoncfg"
 
+if [ "$INSTALL_TARGET" = "lenovo" ]; then
+  ensure_omarchy_plugin "jankeesvw.time-machine" "jankeesvw/omarchy-time-machine"
+fi
+
 unset -f ensure_omarchy_plugin
