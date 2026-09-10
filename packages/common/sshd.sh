a3794da9b749e30d2d16dbf49c3e9430603fb437
Pierre Olivier Martel <pomartel@coderubik.com>
2026-09-10 09:59:43 -0400

    Replace OpenSSH setup with Tailscale SSH

--- packages/common/sshd.sh
+++
@@ -1,4 +0,0 @@
-if ! systemctl is-enabled --quiet sshd.service ||
-  ! systemctl is-active --quiet sshd.service; then
-  omarchy setup security sshd
-fi
--- packages/common/tailscale.sh
+++ packages/common/tailscale.sh
@@ -1,3 +1,5 @@
 if omarchy-cmd-missing tailscale; then
   omarchy install service tailscale
 fi
+
+tailscale set --ssh
