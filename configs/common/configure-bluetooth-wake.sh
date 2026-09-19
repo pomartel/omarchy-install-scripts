c1d64599ff192d3845ddf3681a33437c0bea7b87
Pierre Olivier Martel <pomartel@coderubik.com>
2026-09-19 14:55:02 -0400

    Disable Bluetooth wake

--- configs/common/configure-bluetooth-wake.sh
+++ configs/common/configure-bluetooth-wake.sh
@@ -3,7 +3,7 @@
 bluetooth_path=$(readlink -f /sys/class/bluetooth/hci0/device 2>/dev/null || true)
 
 if [[ -z "$bluetooth_path" ]]; then
-  echo "Bluetooth controller hci0 not found; skipping wake configuration."
+  echo "Bluetooth controller hci0 not found; skipping wake disable configuration."
 else
   usb_path="$bluetooth_path"
   while [[ "$usb_path" != "/" && ! -f "$usb_path/idVendor" ]]; do
@@ -21,7 +21,7 @@ else
   if [[ -f "$usb_path/idVendor" && -f "$usb_path/idProduct" && -e "$usb_path/power/wakeup" ]]; then
     usb_vendor=$(<"$usb_path/idVendor")
     usb_product=$(<"$usb_path/idProduct")
-    wake_rule=$(printf 'ACTION=="add|change", SUBSYSTEM=="usb", ATTR{idVendor}=="%s", ATTR{idProduct}=="%s", ATTR{power/wakeup}="enabled"' "$usb_vendor" "$usb_product")
+    wake_rule=$(printf 'ACTION=="add|change", SUBSYSTEM=="usb", ATTR{idVendor}=="%s", ATTR{idProduct}=="%s", ATTR{power/wakeup}="disabled"' "$usb_vendor" "$usb_product")
   else
     wake_rule=""
   fi
@@ -30,7 +30,7 @@ else
     pci_vendor=$(<"$pci_path/vendor")
     pci_device=$(<"$pci_path/device")
     [[ -n "$wake_rule" ]] && wake_rule+=$'\n'
-    wake_rule+=$(printf 'ACTION=="add|change", SUBSYSTEM=="pci", ATTR{vendor}=="%s", ATTR{device}=="%s", ATTR{power/wakeup}="enabled"' "$pci_vendor" "$pci_device")
+    wake_rule+=$(printf 'ACTION=="add|change", SUBSYSTEM=="pci", ATTR{vendor}=="%s", ATTR{device}=="%s", ATTR{power/wakeup}="disabled"' "$pci_vendor" "$pci_device")
   fi
 
   if [[ -z "$wake_rule" ]]; then
@@ -38,11 +38,11 @@ else
   else
     printf '%s\n' "$wake_rule" | sudo tee /etc/udev/rules.d/70-bluetooth-keyboard-wake.rules >/dev/null
     if [[ -e "$usb_path/power/wakeup" ]]; then
-      echo enabled | sudo tee "$usb_path/power/wakeup" >/dev/null
+      echo disabled | sudo tee "$usb_path/power/wakeup" >/dev/null
     fi
 
     if [[ "$pci_path" != "/" && -e "$pci_path/power/wakeup" ]]; then
-      echo enabled | sudo tee "$pci_path/power/wakeup" >/dev/null
+      echo disabled | sudo tee "$pci_path/power/wakeup" >/dev/null
     fi
 
     sudo udevadm control --reload-rules
